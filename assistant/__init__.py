0465f23097cecea5109bfdfc2ce9a3df15960f1a
minkxx <aryuokk@gmail.com>
2023-06-18 23:00:20 +0530

    Initial commit

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -8,7 +8,6 @@ asstb = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="assistant\modules")
     )
 
 asstb.start()
--- assistant/__main__.py
+++ assistant/__main__.py
@@ -5,11 +5,30 @@ from assistant import (
     BOT_USERNAME,
     asstb,
 )
+from assistant.modules import ALL_MODULES
 from config import LOG_GROUP
 
 loop = asyncio.get_event_loop()
 
 async def start_bot():
+    for module in ALL_MODULES:
+        imported_module = importlib.import_module("assistant.modules." + module)
+    bot_modules = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+    print("+===============================================================+")
+    print("|                         ASSISTANT BOT                         |")
+    print("+===============+===============+===============+===============+")
+    print(bot_modules)
+    print("+===============+===============+===============+===============+")
+    print(f"BOT STARTED AS {BOT_NAME}!")
+    print(f"USERBOT STARTED AS {USERBOT_NAME}!")
     print("Sending online status!")
     await asstb.send_message(LOG_GROUP, "Bot started!")
     await idle()
