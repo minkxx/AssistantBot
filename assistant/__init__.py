5a45a408aeb350cb4857bdf06c709cb59405f7e6
minkxx <aryuokk@gmail.com>
2023-06-19 07:37:45 +0530

    Initial Commit

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,7 +1,13 @@
+import sys
 import pyroaddon
 from pyrogram import Client
 
 from config import (API_ID, API_HASH, BOT_TOKEN)
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 7:
+    print("You MUST have a python version of at least 3.7! Multiple features depend on this. Bot quitting.")
+    sys.exit()
 
 asstb = Client(
     name = "annon",
