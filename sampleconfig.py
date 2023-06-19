ef1bac62e6142f5ed1fc2d3173fc8c60a19f4cf6
Minkxx <114978467+minkxx@users.noreply.github.com>
2023-06-19 15:27:28 +0530

    + render support

--- sampleconfig.py
+++ sampleconfig.py
@@ -1,14 +1,14 @@
 import os
 
 # Your app id and app hash from my.telegram.org
-API_ID = 1
-API_HASH = ""
+API_ID = int(os.environ.get("API_ID", 1))
+API_HASH = os.environ.get("API_HASH", None)
 
 # Your bot token from t.me/BotFather
-BOT_TOKEN = ""
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 # Get your id from t.me/MissRose_bot /id
-OWNER_ID = 1
+OWNER_ID = int(os.environ.get("OWNER_ID", 1))
 
 # Add t.me/MissRose_bot to your log group and type /id to get groupid
-LOG_GROUP = 1
+LOG_GROUP = int(os.environ.get("LOG_GROUP", 1))
