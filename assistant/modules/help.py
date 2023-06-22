29904881a2a0297444f27b5a92ddb6e9929bc997
minkxx <aryuokk@gmail.com>
2023-06-22 14:28:45 +0530

    add : plugin (pmbot)

--- assistant/modules/help.py
+++ assistant/modules/help.py
@@ -27,7 +27,9 @@ async def help_cmd(c : asstb, m : Message):
     else:
         main_text = f"Hey {m.chat.first_name}, I,m a simple assistant bot more like a No-PM's bot.\nCheck buttons for help or you can seek help in Support Group\n\nGeneral commands:\n/start - starts bot\n/help - this help menu"
         main_keyboard = ikb([
-            [("Alive", "alive_callback"), ("Session Gen", "session_callback"), ("Whois", "whois_callback")]
+            [("Alive", "alive_callback"), ("PM Bot", "pmbot_callback"), ("Session Gen", "session_callback")],
+            [("Whois", "whois_callback")],
+            
         ])
         await c.send_message(
             chat_id = m.chat.id,
@@ -41,7 +43,7 @@ async def chat_query(c : asstb, qb : CallbackQuery):
     if qb.data == "alive_callback":
         text = f'''**Alive Command**
 
-/alive - `to check stats`
+/alive - `to check status`
 /ping - `to check my speed`
 '''
         keyboard = ikb([
@@ -67,6 +69,18 @@ To fetch user id or chat id:
         text = f'''**SessionGen Command**
         
 /ssgen - `generates a pyrogram user session`
+'''
+        keyboard = ikb([
+            [("Back", "back_callback")]
+        ])
+        await qb.edit_message_text(text, reply_markup=keyboard)
+
+    elif qb.data == "pmbot_callback":
+        text = f'''**PM Bot Command**
+        
+-> Owner can receive messages from users
+-> Users can send messages to owner without bothering him in pm
+
 '''
         keyboard = ikb([
             [("Back", "back_callback")]
--- assistant/modules/start.py
+++ assistant/modules/start.py
@@ -6,15 +6,16 @@ from assistant import (asstb, OWNER_USERNAME)
 
 @asstb.on_message(filters.command("start"))
 async def start_cmd(c : asstb, m : Message):
+        _img = "https://graph.org/file/6d37af28a21b094079ff5.jpg"
         txt = f"Hey! {m.from_user.first_name} gald to see you here. I'm @{OWNER_USERNAME}'s personal assistant bot. You can contact him through me!\n\n/help - to get my help menu"
         keyboard = ikb([
     [("Dev", "t.me/minkxx69", "url"), ("Github", "https://github.com/minkxx", "url"), ("Repo", "https://github.com/AssistantBot", "url")],
     [("Channel", "https://t.me/MinkxxSays", "url"), ("Group", "https://t.me/BotsUnion", "url")]
 ])
-        await c.send_message(
+        await c.send_photo(
             chat_id=m.chat.id,
-            text=txt,
+            photo = _img,
+            caption = txt,
             reply_to_message_id = m.id,
-            reply_markup=keyboard, 
-            disable_web_page_preview=True,
+            reply_markup=keyboard,
             )
