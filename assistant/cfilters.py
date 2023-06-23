8bb30fe1925488da39aa64ad0b119225c181345e
minkxx <aryuokk@gmail.com>
2023-06-23 17:46:24 +0530

    add : custom filters

---
+++ assistant/cfilters.py
@@ -0,0 +1,8 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from assistant import OWNER_ID
+
+async def only_owner(_, __, m : Message):
+    return bool(m.from_user.id == OWNER_ID)
+
+owner = filters.create(only_owner)
